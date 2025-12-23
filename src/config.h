7d0f631194f24b8456524b8ff1380598edcea42e
Ralsus <roftogus@gmail.com>
2025-12-23 21:18:15 +0300

    qwewqe
    wqeqweqew

---
+++ src/config.h
@@ -0,0 +1,37 @@
+// config.h - Ring Racers Android build configuration
+// Generated for Android NDK build
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// Version info
+#define SRB2VERSION "2"
+#define SRB2VERSIONSTRING "v2.3"
+#define VERSIONSTRING "v2.3"
+#define VERSIONSTRINGW L"v2.3"
+
+// Build info used by comptime.c
+#define SRB2_COMP_BRANCH "android-port"
+#define SRB2_COMP_REVISION "android"
+#define SRB2_COMP_NOTE ""
+#define CMAKE_BUILD_TYPE "Release"
+#define SRB2_COMP_OPTIMIZED 1
+
+// Legacy defines
+#define COMPREVISION "android"
+#define COMPBRANCH "android-port"
+#define COMPNOTE ""
+#define COMPDATE __DATE__
+#define COMPTIME __TIME__
+
+// Asset hashes (empty for Android)
+#define ASSET_HASH_BIOS_PK3 ""
+#define ASSET_HASH_CHARS_PK3 ""
+#define ASSET_HASH_MAPS_PK3 ""
+#define ASSET_HASH_TEXTURES_PK3 ""
+
+// Git info
+#define COMPGITBRANCH ""
+#define COMPGITHASH ""
+
+#endif // __CONFIG_H__
